0c201dd36a116018c2522f43c3e403edaac093f4
Ali <100287885+AlisahibAmiraslan@users.noreply.github.com>
2024-01-28 14:19:36 +0300

    update comment for code

--- Login.cs
+++ Login.cs
@@ -17,7 +17,7 @@ namespace HotelManagmentSystem
         {
             InitializeComponent();
         }
-
+        // sql connecttion
         SqlConnection Con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=HotelManagment;Integrated Security=True;Connect Timeout=30;Encrypt=False;");
         private void loginExitlbl_Click(object sender, EventArgs e)
         {
